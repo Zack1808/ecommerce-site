8f9635dbf7abc1374942000239ab2b91a3a778b1
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-10-03 18:39:20 +0200

    Fetching store items using commerce.js

---
+++ src/api/commerce.js
@@ -0,0 +1,5 @@
+import Commerce from "@chec/commerce.js";
+
+const API_KEY = "pk_test_47417e81ca1a6a83980737d91e3a3d312e4d3b928d63e"
+
+export const commerce = new Commerce(API_KEY, true);
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Importing the costume made components
 import Products from './Products';
 import Navbar from './Navbar';
+
+// importing the commerce api
+import { commerce } from '../api/commerce'
 
 // Importing the style file
 import "../css/App.css";
 
 // Creating the App component
 const App = () => {
+
+    const [products, setProducts] = useState([]); 
+
+    // Will run the function that will fetch the products
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    // Function that will fetch the products
+    const fetchProducts = async() => {
+        const { data } = await commerce.products.list();
+        setProducts(data)
+    }
+
+    console.log(products)
+
     return (
         <div className='container'>
             <Navbar />
