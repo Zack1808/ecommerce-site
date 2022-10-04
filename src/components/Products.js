b34867a7dc645bc1dd049291dbd62cd83b9aa325
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-10-04 10:03:38 +0200

    Fixed the size of the item card in the cart

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 // Importing the costume made components
 import Product from './Product';
+import Loader from './Loader'
 
 // Importing the style file 
 import "../css/Products.css"
 
 // Creating the Products component
 const Products = ({ products, onAddToCart }) => {
+
+    console.log(products)
+
+    if(products.length === 0) return <Loader />
+
     return (
         <div className='product-container'>
             {
